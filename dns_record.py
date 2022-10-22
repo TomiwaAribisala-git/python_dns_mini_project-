a031a28bb729aa57c2ac2a4102f74a92b49f97b0
TomiwaAribisala-git <tomiwaaribisala@gmail.com>
2022-10-22 19:08:00 +0100

    updated README

--- dns_record.py
+++ dns_record.py
@@ -3,22 +3,22 @@
 #Import dns resolver class from dnspython library 
 import dns.resolver 
 
-# User defined website
-website = input("Enter the name of the website: ")
 # Dictionary to store the dns record of a website
 dns_record = {}
-def dns_record(website): 
-    """Fetch the DNS Record and MX Record of a given website"""
-    # Fetching the 'A' record of the website and storing it in the directory
-    a_record = dns.resolver.resolve(website, 'A')
-    for ipaddr in a_record:
-        dns_record['A_Record_IP'] = ipaddr
-    # Fetch MX record(DNS Email Client Service) of a website
-    mx_record = dns.resolver.resolve(website, 'MX')
-    for service in mx_record:
-        dns_record['MX_Record'] = service 
 
-    # Displaying the A record and MX record
-    for key,value in dns_record.items():
-        print(f"{key}: {value}")
-    
+# User defined website
+website = input("Enter the name of the website: ")
+
+# Fetching the 'A' record of the website and storing it in the directory
+a_record = dns.resolver.resolve(website, 'A')
+for ipaddr in a_record:
+    dns_record['A_Record_IP'] = ipaddr
+
+# Fetch MX record(DNS Email Client Service) of a website
+mx_record = dns.resolver.resolve(website, 'MX')
+for service in mx_record:
+    dns_record['MX_Record'] = service 
+
+# Displaying the A record and MX record
+for key,value in dns_record.items():
+    print(f"{key}: {value}") 
